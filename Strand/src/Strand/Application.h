74fc332970848c820aef75e2f87c6a1f59c0fed4
AyubGM <navnlosking@gmail.com>
2025-08-08 12:32:27 +0200

    VAO

--- Strand/src/Strand/Application.h
+++ Strand/src/Strand/Application.h
@@ -10,6 +10,7 @@
 #include "Strand/ImGui/ImGuiLayer.h"
 #include "Strand/Renderer/Shader.h"
 #include "Strand/Renderer/Buffer.h"
+#include "Strand/Renderer/VertexArray.h"
 
 
 namespace Strand {
@@ -38,11 +39,11 @@ namespace Strand {
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 
-		std::unique_ptr<Shader> m_Shader;
+		std::shared_ptr<Shader> m_Shader;
+		std::shared_ptr<VertexArray> m_VertexArray;
 
-		unsigned int m_VertextArray;
-		std::unique_ptr<VertexBuffer> m_VertexBuffer;
-		std::unique_ptr<IndexBuffer> m_IndexBuffer;
+		std::shared_ptr<Shader> m_BlueShader;
+		std::shared_ptr<VertexArray> m_SquareVA;
 
 	private:
 		static Application* s_Instance;
