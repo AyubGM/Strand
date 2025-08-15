975edfaf6055c4db832e5935953e214b136633fc
AyubGM <navnlosking@gmail.com>
2025-08-15 15:35:21 +0200

    2D Transforms  and Textures

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -13,7 +13,7 @@ Sandbox2D::Sandbox2D()
 
 void Sandbox2D::OnAttach()
 {
-	
+	m_CheckerboardTexture = Strand::Texture2D::Create("assets/textures/Checkerboard.png");
 }
 
 void Sandbox2D::OnDetach()
@@ -30,13 +30,12 @@ void Sandbox2D::OnUpdate(Strand::Timestep ts)
 	Strand::RenderCommand::Clear();
 
 	Strand::Renderer2D::BeginScene(m_CameraController.GetCamera());
-	Strand::Renderer2D::DrawQuad({ 0.0f, 0.0f }, { 1.0f, 1.0f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Strand::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+	Strand::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
+	Strand::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
 	Strand::Renderer2D::EndScene();
 
 	Strand::Renderer::EndScene();
-	// TODO: Add these functions - Shader::SetMat4, Shader::SetFloat4
-	// std::dynamic_pointer_cast<Strand::OpenGLShader>(m_FlatColorShader)->Bind();
-	// std::dynamic_pointer_cast<Strand::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
 }
 
 void Sandbox2D::OnImGuiRender()
--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -23,5 +23,7 @@ private:
 	Strand::Ref<Strand::VertexArray> m_SquareVA;
 	Strand::Ref<Strand::Shader> m_FlatColorShader;
 
+	Strand::Ref<Strand::Texture2D> m_CheckerboardTexture;
+
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
 };
