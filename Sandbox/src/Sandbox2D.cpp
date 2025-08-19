57c3aca417dab7d7061cd2dda00e2b620a77950b
AyubGM <navnlosking@gmail.com>
2025-08-19 22:39:37 +0200

    Renderer Stats

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -40,7 +40,8 @@ void Sandbox2D::OnUpdate(Strand::Timestep ts)
 		Strand::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
 		Strand::RenderCommand::Clear();
 	}
-
+	// Render
+	Strand::Renderer2D::ResetStats();
 	{
 		static float rotation = 0.0f;
 		rotation += ts * 50.0f;
@@ -50,8 +51,19 @@ void Sandbox2D::OnUpdate(Strand::Timestep ts)
 		Strand::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Strand::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Strand::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Strand::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		Strand::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
 		Strand::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
+		Strand::Renderer2D::EndScene();
+
+		Strand::Renderer2D::BeginScene(m_CameraController.GetCamera());
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		{
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			{
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f };
+				Strand::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+			}
+		}
 		Strand::Renderer2D::EndScene();
 	}
 
@@ -62,6 +74,14 @@ void Sandbox2D::OnImGuiRender()
 	SD_PROFILE_FUNCTION();
 
 	ImGui::Begin("Settings");
+
+	auto stats = Strand::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 
 	ImGui::End();
