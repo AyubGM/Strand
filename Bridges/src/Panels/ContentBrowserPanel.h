c61c14c56f48445688d72bcbf28c8ba5849d1c48
AyubGM <navnlosking@gmail.com>
2025-09-03 22:46:37 +0200

    Icons

--- Bridges/src/Panels/ContentBrowserPanel.h
+++ Bridges/src/Panels/ContentBrowserPanel.h
@@ -2,6 +2,8 @@
 
 #include <filesystem>
 
+#include "Strand/Renderer/Texture.h"
+
 namespace Strand {
 
 	class ContentBrowserPanel
@@ -12,6 +14,9 @@ namespace Strand {
 		void OnImGuiRender();
 	private:
 		std::filesystem::path m_CurrentDirectory;
+
+		Ref<Texture2D> m_DirectoryIcon;
+		Ref<Texture2D> m_FileIcon;
 	};
 
 }
