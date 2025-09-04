0b2d5e00e79f5b41e6ed17912a24e9a998106d2e
AyubGM <navnlosking@gmail.com>
2025-09-04 20:07:23 +0200

    Stability + bug fixes Added IsLoaded to Texture to see if texture data was actually loaded Added .strand extension checking to EditorLayer::OpenScene

--- Bridges/src/Panels/SceneHierarchyPanel.cpp
+++ Bridges/src/Panels/SceneHierarchyPanel.cpp
@@ -332,7 +332,13 @@ namespace Strand {
 					{
 						const wchar_t* path = (const wchar_t*)payload->Data;
 						std::filesystem::path texturePath = std::filesystem::path(g_AssetPath) / path;
-						component.Texture = Texture2D::Create(texturePath.string());
+						Ref<Texture2D> texture = Texture2D::Create(texturePath.string());
+						/*wchar_t path[260] = {};
+						memcpy(path, payload->Data, payload->DataSize);*/
+						if (texture->IsLoaded())
+							component.Texture = texture;
+						else
+							SD_WARN("Could not load texture {0}", texturePath.filename().string());
 					}
 					ImGui::EndDragDropTarget();
 				}
