62fdfa3bf86f2f281bc4caf3209f26e9cf7223a2
AyubGM <navnlosking@gmail.com>
2025-09-04 20:53:25 +0200

    Log

--- scripts/Setup.py
+++ scripts/Setup.py
@@ -14,6 +14,10 @@ os.chdir('./../') # Change from devtools/scripts directory to root
 
 VulkanRequirements.Validate()
 
+
+print("\nUpdating submodules...")
+subprocess.call(["git", "submodule", "update", "--init", "--recursive"])
+
 if platform.system() == "Windows":
     print("\nRunning premake...")
     subprocess.call([os.path.abspath("./scripts/Win-GenProjects.bat"), "nopause"])
